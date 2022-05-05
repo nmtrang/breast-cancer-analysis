97027f021458a832cf3b52d817649229d60428f9
nmtrang <pneppy@gmail.com>
2022-05-05 19:42:04 +0700

    fixed input values for prediction

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pandas as pd
-import plotly
+import numpy as np
+from sklearn.preprocessing import LabelEncoder
+import pickle
 
 
 st.image('https://www.agrisafe.org/wp-content/uploads/2021/07/breast-cancer-wyntk.jpg')
@@ -10,24 +12,52 @@ df = pd.read_csv('./data/breast-cancer-final.csv', index_col=[0])
 st.subheader('Original dataset')
 st.dataframe(df)
 
-# cat_attr = ['menopause', 'node-caps', 'breast', 'breast-quad', 'irradiat']
-# bin_attr = ['age', 'tumor-size', 'inv-nodes']
-
 attr_desc = {
-    'age': 'Age of the patient',
-    'menopause': 'Type of menopause',
-    'tumor-size': 'The size of the tumor',
+    'menopause': "the time that marks the end of a person's menstrual cycle.",
+    'age': 'the age of the patient.',
+    'tumor-size': 'the size of the tumor in the patient.',
+    'inv-nodes': 'the number of axillary lymph nodes that contain metastatic breast cancer visible on histological examination.',
+    'node-caps': 'node cap',
+    'deg-malig': 'the histological grade of the tumor. Tumors that a grade 1 predominantly consist of cells that, while neoplastic, retain many of their usual characteristics. Grade 3 tumors predominately consist of cells that are highly abnormal.',
+    'breast': 'the side of breast that the patient has had the most malignant tumor, may occur in either breast.',
+    'breast-quad': 'the quadrant of the breast that the patient has had the most malignant tumor.',
+    'irradiat': 'whether the patient has had radiation therapy to destroy cancer cells.'
 }
 
-st.plotly_chart(figure_or_data=plotly.graph_objs.Figure, use_container_width=False,sharing='streamlit', **kwargs)
 
+st.subheader('Attributes description')
+st.write(attr_desc)
 
 st.subheader('Prediction section')
 
-with st.form('Input information: '):
-    for attribute in df.columns.tolist()[1:]:
-        st.selectbox(f'Select {attribute}', df[attribute].unique().tolist())
-    
-    predict_button = st.form_submit_button('Predict')
-    
-st.subheader('Results here!')
+selected_answer = [st.selectbox(f'Select {column}', df[column].unique(
+).tolist()) for column in df.columns.tolist()[1:]]
+
+
+model = pickle.load(open('./svm_model', 'rb'))
+
+
+encoder = LabelEncoder()
+i = 0
+encoded_answer = []
+
+for attr in df.iloc[:, 1:]:
+    encoder.fit(df[attr])
+    label_mapping = dict(
+        zip(encoder.classes_, encoder.transform(encoder.classes_)))
+    encoded_answer.append(label_mapping.get(selected_answer[i]))
+    i += 1
+
+
+def predict_result(*array_to_predict):
+    input = np.array([encoded_answer]).astype(np.float64)
+    prediction = model.predict(input)
+
+    if prediction == 'no-recurrence-events':
+        st.write('This patient will not have breast cancer recurrence')
+    else:
+        st.write('This patient will have breast cancer recurrence')
+
+
+if st.button('Predict'):
+    predict_result(encoded_answer)
